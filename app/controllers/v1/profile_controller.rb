5b7ba0c23887d260ab4106e816efdb87284d5c6e
Khale <lkha.5023@gmail.com>
2021-04-27 03:10:41 +0700

    Update profile uiii

--- app/controllers/v1/profile_controller.rb
+++ app/controllers/v1/profile_controller.rb
@@ -1,5 +1,8 @@
 class V1::ProfileController < ApplicationController
   def index
+    @select = Post.select(:id,:user_id,:username,:title,:selectedImgFile,:selectedVidFile,:selectedAudFile,:created_at).joins(:user).order(created_at: :desc)
     @post = Post.select(:id,:username,:title,:selectedImgFile,:selectedVidFile,:selectedAudFile,:created_at).joins(:user).order(created_at: :desc)
+    @user = User.select(:id,:username,:profilePicture)
+    @comment = Comment.select(:id,:username,:text,:post_id,:user_id).joins(:user).order(created_at: :desc)
   end
 end
